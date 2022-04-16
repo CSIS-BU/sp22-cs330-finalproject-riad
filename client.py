dca4ddbf617ab1901b076de996b0ff608f75b26d
rabboud <rabboud@mail.bradley.edu>
2022-04-15 22:29:49 -0500

    Client skeleton

---
+++ client.py
@@ -0,0 +1,30 @@
+import sys
+
+def getUserInput():
+	"""Function geto user input?"""
+
+	# Read the user's input
+	# Make sure it's within the expected number range
+	# If acceptable, return back to client
+
+	pass
+
+def client():
+	"""Connection to server."""
+
+	# Connection handling here, maybe another function for receiving user input?
+	# It would be cleaner that way.
+
+	pass
+
+
+def main():
+	"""Main entry function."""
+
+	# Probably just going to accept a single argument as the port to connect to.
+	if len(sys.argv) > 0:
+		sys.exit("Not ready yet!")
+
+
+if __name__ == "__main__":
+	main()
