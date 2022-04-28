9fc26e86a42a1814562d6ea8fe3134b23abf7449
rabradley <rabboud@mail.bradley.edu>
2022-04-28 10:41:27 -0500

    Multithreading support. Now handles concurrent client connections.

--- server.py
+++ server.py
@@ -2,9 +2,62 @@ import sys
 import socket
 import shared
 import random
+import threading
 from shared import PacketType, receivePacket, send, receive
 
 clients = {}
+
+def handleClient(client, address):
+	with client:
+		print(client)
+		# A client has connected, ask them for the number range they want.
+		# It should still be within our predefined values in config.
+		send(client, PacketType.ASK_CLIENT_MIN_MAX, shared.MIN_NUMBER, shared.MAX_NUMBER)
+
+		#############################################################################################################################
+		# The client has sent back the min and max they want.
+		data = receivePacket(client, PacketType.GIVE_SERVER_MIN_MAX)
+		# Verify what we got to be sure it fits our bounds. If not, kill the connection.
+		if data[0] < shared.MIN_NUMBER or data[1] > shared.MAX_NUMBER:
+			print("Client sent invalid min & max.")
+			client.close()
+		else:
+			# They're valid.
+			clients[client] = {
+				"correctNumber": random.randint(data[0], data[1]),
+				"minimum": data[0],
+				"maximum": data[1],
+				"guessHistory": []
+			}
+			print("Client {} chose {} and {} as their min & max.".format(client.fileno(), data[0], data[1]))
+		
+		# Tell them to start guessing.
+		send(client, PacketType.START_GUESSING)
+		#############################################################################################################################
+		while True:
+			# We should be receiving guesses now.
+			data = receivePacket(client, PacketType.GUESS)
+			if not data[0]:
+				# Invalid response. Close the connection.
+				client.close()
+			
+			clientData = clients[client]
+			clientData["guessHistory"].append(data[0])
+
+			if data[0] == clientData["correctNumber"]:
+				print("They got it right! It only took {} guesses.".format(len(clientData["guessHistory"])))
+				send(client, PacketType.GUESS_CORRECT)
+				break
+			elif data[0] < clientData["minimum"]:
+				print("They sent a number below their previously defined minimum, despite safeguards. Kill the connection.")
+				client.close()
+			elif data[0] > clientData["maximum"]:
+				print("They sent a number above their previously defined maximum, despite safeguards. Kill the connection.")
+				client.close()
+			else:
+				# Incorrect guess.
+				send(client, PacketType.GUESS_INCORRECT)
+
 
 def doServer(server_port):
 	"""Connection logic handler."""
@@ -20,54 +73,7 @@ def doServer(server_port):
 			while True:
 				(client, address) = server.accept()
 
-				with client:
-					# A client has connected, ask them for the number range they want.
-					# It should still be within our predefined values in config.
-					send(client, PacketType.ASK_CLIENT_MIN_MAX, shared.MIN_NUMBER, shared.MAX_NUMBER)
-
-					#############################################################################################################################
-					# The client has sent back the min and max they want.
-					data = receivePacket(client, PacketType.GIVE_SERVER_MIN_MAX)
-					# Verify what we got to be sure it fits our bounds. If not, kill the connection.
-					if data[0] < shared.MIN_NUMBER or data[1] > shared.MAX_NUMBER:
-						print("Client sent invalid min & max.")
-						client.close()
-					else:
-						# They're valid.
-						clients[client] = {
-							"correctNumber": random.randint(data[0], data[1]),
-							"minimum": data[0],
-							"maximum": data[1],
-							"guessHistory": []
-						}
-						print("Client chose {} and {} as their min & max.".format(data[0], data[1]))
-					
-					# Tell them to start guessing.
-					send(client, PacketType.START_GUESSING)
-					#############################################################################################################################
-					while True:
-						# We should be receiving guesses now.
-						data = receivePacket(client, PacketType.GUESS)
-						if not data[0]:
-							# Invalid response. Close the connection.
-							client.close()
-						
-						clientData = clients[client]
-						clientData["guessHistory"].append(data[0])
-
-						if data[0] == clientData["correctNumber"]:
-							print("They got it right! It only took {} guesses.".format(len(clientData["guessHistory"])))
-							send(client, PacketType.GUESS_CORRECT)
-							break
-						elif data[0] < clientData["minimum"]:
-							print("They sent a number below their previously defined minimum, despite safeguards. Kill the connection.")
-							client.close()
-						elif data[0] > clientData["maximum"]:
-							print("They sent a number above their previously defined maximum, despite safeguards. Kill the connection.")
-							client.close()
-						else:
-							# Incorrect guess.
-							send(client, PacketType.GUESS_INCORRECT)
+				threading.Thread(target = handleClient, args = (client,address)).start()
 
 		except KeyboardInterrupt:
 			print("Server cancelled. Exiting.")
